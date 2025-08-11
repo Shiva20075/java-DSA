527f6b980c6aa4dada3211c98b1d6772ccab482e
shiva20075 <palleshiva2007@gmail.com>
2025-08-11 23:04:50 +0530

    Prints a Triangel valley upside and DownSide

---
+++ src/TriangleUpsideDown.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class TriangleUpsideDown {
+
+    /**
+     * This
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Scanner z = new Scanner(System.in);
+        System.out.print("ENTER A NUMBER -> ");
+        int userInput = z.nextInt();
+
+        for ( int i = 1; i <= userInput; i++){
+
+            for (int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            for ( int s = 1; s <= 2*(userInput - i); s++){
+                System.out.print(" ");
+            }
+            for ( int k = 1; k <= i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for ( int i = 1; i <= userInput; i++){
+
+            for (int j = 1; j <= userInput - i; j++){
+                System.out.print("*");
+            }
+            for (int s = 1; s <= 2*i; s++){
+                System.out.print(" ");
+            }
+            for ( int k = 1; k <= userInput - i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
