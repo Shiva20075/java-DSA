49162e77521216d4321f469db8539d6a6100bfd1
pallesai <130438696+pallesai@users.noreply.github.com>
2025-08-12 22:10:11 -0700

    Fix Spacing and indentation

--- src/TriangleUpsideDown.java
+++ src/TriangleUpsideDown.java
@@ -12,31 +12,36 @@ public class TriangleUpsideDown {
         System.out.print("ENTER A NUMBER -> ");
         int userInput = z.nextInt();
 
-        for ( int i = 1; i <= userInput; i++){
+        for (int i = 1; i <= userInput; i++) {
 
-            for (int j = 1; j <= i; j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            for ( int s = 1; s <= 2*(userInput - i); s++){
+            
+            for (int s = 1; s <= 2*(userInput - i); s++) {
                 System.out.print(" ");
             }
-            for ( int k = 1; k <= i; k++){
+            
+            for (int k = 1; k <= i; k++) {
                 System.out.print("*");
             }
+            
             System.out.println();
         }
 
-        for ( int i = 1; i <= userInput; i++){
-
-            for (int j = 1; j <= userInput - i; j++){
+        for (int i = 1; i <= userInput; i++) {
+            for (int j = 1; j <= userInput - i; j++) {
                 System.out.print("*");
             }
-            for (int s = 1; s <= 2*i; s++){
+            
+            for (int s = 1; s <= 2*i; s++) {
                 System.out.print(" ");
             }
-            for ( int k = 1; k <= userInput - i; k++){
+            
+            for (int k = 1; k <= userInput - i; k++) {
                 System.out.print("*");
             }
+            
             System.out.println();
         }
     }
