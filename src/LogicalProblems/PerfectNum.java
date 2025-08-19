5a13d04d3c3a16c44f69a31070ab8699ecc1183f
shiva20075 <palleshiva2007@gmail.com>
2025-08-19 20:01:55 +0530

    Added Logic Inside Method

--- src/LogicalProblems/PerfectNum.java
+++ src/LogicalProblems/PerfectNum.java
@@ -3,28 +3,41 @@ import java.util.Scanner;
 
 public class PerfectNum {
 
-    public static void main(String[] args) {
+    /**
+     * Checks if a number is a perfect number.
+     * A perfect number is a positive integer that equals the sum of its proper divisors,
+     * which are the positive divisors of the number excluding the number itself.
+     *
+     * For example, 6 is a perfect number because its proper divisors are 1, 2, and 3,
+     * and 1 + 2 + 3 = 6.
+     *
+     */
+    public static boolean isPerfectNumber(int n) {
 
-        /**
-         * This class takes input from user and print if the input number is perfect Number or not.
-         * EX: input -> 6
-         * Output -> 6 is a perfect Number.
-         */
-
-        Scanner s = new Scanner(System.in);
-        System.out.print("ENTER A NUMBER TO CHECK PERFECT NUM -> ");
-        int n = s.nextInt();
         int sum = 0;
-
         for (int i = 1; i < n; i++) {
             if (n % i == 0) {
                 sum += i;
             }
         }
 
+        boolean isPerfect;
         if (sum == n) {
-            System.out.println(n + " IS A PERFECT NUMBER");
+            isPerfect = true;
+        } else {
+            isPerfect = false;
+        }
 
+        return isPerfect;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("ENTER A NUMBER TO CHECK PERFECT NUM -> ");
+        int n = s.nextInt();
+
+        if (isPerfectNumber(n)) {
+            System.out.println(n + " IS A PERFECT NUMBER");
         } else {
             System.out.println(n + " IS NOT A PERFECT NUMBER");
         }
